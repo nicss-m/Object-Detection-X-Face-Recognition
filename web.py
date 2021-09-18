c85fefc4d0d6431871cb42c10754a7ef39eb2709
nicss-m <78718440+nicss-m@users.noreply.github.com>
2021-09-18 20:57:18 +0800

    Update web.py

--- web.py
+++ web.py
@@ -64,6 +64,7 @@ def loading():
     # load credentials
 #     load_dotenv()
 #     CREDENTIALS = json.loads(os.environ.get('CREDENTIALS'))
+    st.write("My Credentials:", st.secrets["CREDENTIALS"])
     CREDENTIALS = st.secrets["CREDENTIALS"]
     
     if os.path.exists('credentials.json'):
